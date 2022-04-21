cc76b7f47d939c3bda4e860c72d9aa5ecb2f9707
Bastien <bastien.esnault@orange.fr>
2022-04-21 17:18:19 +0200

    Ajout fichiers tests

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,31 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+    public function test_empty() {
+        $tempTracker = new TempTracker();
+        $this->assertEmpty($tempTracker->get_temps());
     }
+    
+    public function test_return_value_inserted() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(76);
+        $this->assertContains(76, $tempTracker->get_temps());
+    }
+    
+    public function test_min() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(15);
+        $tempTracker->insert(78);
+        $tempTracker->insert(34);
+        $this->assertEquals(15,$tempTracker->get_min());
+    }
+
+    public function test_max() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(15);
+        $tempTracker->insert(78);
+        $tempTracker->insert(34);
+        $this->assertEquals(78,$tempTracker->get_max());
+    }
+
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,17 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
+    public function test_empty_array()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEmpty(flatten([]));
+    }
+    public function test_insert()
+    {
+        $this->assertIsArray(flatten([7]));
+        $this->assertIsArray(flatten([7, [77], 7]));
+    }
+    public function test_multiple_array()
+    {
+        $this->assertEquals(flatten([[77], 1, 1, [7,7]]), [77,1,1,7,7], "Should be [77,1,1,7,7]");
     }
 }
