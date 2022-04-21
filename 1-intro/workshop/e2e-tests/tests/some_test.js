032a238374ba11af75b5fb0f25ff9c9007075eea
Arthurolivier5 <olivier.arthur99@gmail.com>
2022-04-21 16:22:02 +0200

    e2e-test

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,9 @@
 Feature('Ynov Nantes');
 
-Scenario('Test Ynov Nantes Land Page', ({ I }) => {
-    I.amOnPage('https://www.ynov-nantes.com/');
-});
+    Scenario('Test Ynov Nantes Land Page', ({ I }) => {
+        I.amOnPage('https://www.ynov-nantes.com/');
+        I.click('.cta');
+        I.appendField('.searchfield', 'info');
+        I.click('.search-input-submit');
+        I.waitForText('Bachelor Informatique', '.searchresult');
+    });
