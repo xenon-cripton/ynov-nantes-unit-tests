0e9fcd313526f5eb26ce6daac87035ec44cc9e51
Paul Roussel <paul.roussel-ext@getalma.eu>
2022-04-28 09:51:13 +0200

    TDD/BDD live code

--- 3-test-driven-development/js-BDD/tests/gilded_rose.steps.js
+++ 3-test-driven-development/js-BDD/tests/gilded_rose.steps.js
@@ -10,6 +10,11 @@ defineFeature(feature, test => {
   let inputItems = [];
   let outputItems = [];
 
+  beforeEach(() => {
+    inputItems = [];
+    outputItems = [];
+  });
+
   test('sellIn should decrease:', ({ given, when, then }) => {
     given('I have an Item:', (table) => {
       table.forEach(row => {
@@ -32,6 +37,32 @@ defineFeature(feature, test => {
         expect(outputItems[index].name).toBe(row.Name);
 	      expect(outputItems[index].sellIn).toBe(parseInt(row.SellIn, 10));
 	      expect(outputItems[index].quality).toBe(parseInt(row.Quality, 10));
+      });
+    });
+  });
+
+  test('quality can\'t be less than 0:', ({ given, when, then }) => {
+    given('I have an Item:', (table) => {
+      table.forEach(row => {
+        inputItems.push(new Item(
+          row.Name,
+          row.SellIn,
+          row.Quality,
+        ));
+      });
+    });
+
+    when('a day pass', () => {
+      const gildedRose = new Shop(inputItems);
+      outputItems = gildedRose.updateQuality();
+    });
+
+    then('my item should be like:', table => {
+
+      table.forEach((row, index) => {
+        expect(outputItems[index].name).toBe(row.Name);
+	      expect(parseInt(outputItems[index].sellIn, 10)).toBe(parseInt(row.SellIn, 10));
+	      expect(parseInt(outputItems[index].quality, 10)).toBe(parseInt(row.Quality, 10));
       });
     });
   });
