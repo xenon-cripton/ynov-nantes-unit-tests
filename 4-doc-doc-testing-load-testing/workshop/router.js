ad4d784d1f85320aa6637d9488a670a429ff698d
xenon-cripton <brhulla@gmail.com>
2022-04-29 16:52:36 +0200

    swagger doc api

--- 4-doc-doc-testing-load-testing/workshop/router.js
+++ 4-doc-doc-testing-load-testing/workshop/router.js
@@ -3,9 +3,18 @@ const bodyParser = require('body-parser');
 const { getTodo, postTodo, patchTodo } = require('./toDoController');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+);
 
 app.get('/todo', getTodo);
 app.post('/todo', postTodo);
--- 4-doc-doc-testing-load-testing/workshop/server.js
+++ 4-doc-doc-testing-load-testing/workshop/server.js
@@ -2,7 +2,6 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router')
 
 const mongoose = require('mongoose');
-const ToDo = require('./toDoModel.js').ToDo;
 const DB_URI = 'mongodb://mongo:27017/toDoApp';
 
 mongoose.connect(DB_URI).then(() => {
